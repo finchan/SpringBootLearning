41e3ea880b8fe09643557e68020e2fadc2bea9c5
finchan <finchan@outlook.com>
2020-02-21 14:01:05 +0800

    Spring MVC

---
+++ src/main/java/com/tasche/springbootlearning/springmvc/WebInitializer.java
@@ -0,0 +1,24 @@
+package com.tasche.springbootlearning.springmvc;
+
+import com.tasche.springbootlearning.springmvc.config.MyMVCConfig;
+import org.springframework.web.WebApplicationInitializer;
+import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
+import org.springframework.web.servlet.DispatcherServlet;
+
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRegistration;
+
+public class WebInitializer implements WebApplicationInitializer {
+    @Override
+    public void onStartup(ServletContext servletContext) throws ServletException {
+        AnnotationConfigWebApplicationContext ctx =
+                new AnnotationConfigWebApplicationContext();
+        ctx.register(MyMVCConfig.class);
+        ctx.setServletContext(servletContext);
+
+        ServletRegistration.Dynamic servlet = servletContext.addServlet("dispatcher", new DispatcherServlet(ctx));
+        servlet.addMapping("/");
+        servlet.setLoadOnStartup(1);
+    }
+}
---
+++ src/main/resources/views/index.jsp
@@ -0,0 +1,13 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+         pageEncoding="UTF-8"%>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <title>Insert Title Here</title>
+    </head>
+    <body>
+        <pre>
+            Welcome to Spring MVC world.
+        </pre>
+    </body>
+ </html>
